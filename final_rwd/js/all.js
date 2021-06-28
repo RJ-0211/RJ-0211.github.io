2c4fe135a4434cda76bafa20db32468cce6bae93
RJ <luivy99@gmail.com>
2021-06-29 07:26:03 +0800

    gogogo_rwd

---
+++ final_rwd/js/all.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    //游標移到開啟下拉選單
+    $('.dropdown').mouseenter(function (e) { 
+        e.preventDefault();
+        $('.dropdown_open').slideDown();
+    });
+    //游標離開關閉下拉選單
+    $('.dropdown_open').mouseleave(function (e) { 
+        e.preventDefault();
+        $('.dropdown_open').slideUp();
+    });
+
+    $('.top a').click(function (e) { 
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop:0
+        },1000);
+    });
+
+
+    
+
+});
